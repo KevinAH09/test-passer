c97cf9e6255fce2d47e8813b0e440bccbf341e3a
KevinAH09 <55261485+KevinAH09@users.noreply.github.com>
2022-02-15 14:21:56 -0600

    se crea la tabla transaction
    se agrega la tabla transacion con un foreign key a la tabla users

--- passer-be-test/databases/postgresql.js
+++ passer-be-test/databases/postgresql.js
@@ -4,7 +4,11 @@ const postgresql = newDb();
 
 // create mock data
 postgresql.public.none(`create table users(pk_user integer, name text, status boolean);
-                insert into users values (123, 'Juan', true);`);
+                        alter table users add constraint primerikey_users primary key(pk_user);
+                        insert into users values (123, 'Juan', true);
+                        create table transaction(pk_transaction integer,fk_user integer, description text, amount float);
+                        alter table transaction add constraint foreign_user foreign key(fk_user) references users(pk_user);
+                        insert into transaction values (1, 123, 'transaccion nueva',122)`);
 
 
 module.exports = {
